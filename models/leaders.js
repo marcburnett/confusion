904e379139b3ec9947340f55f05d1f71dd39dc9c
marcburnett <marc_burnett@yahoo.com>
2018-10-14 22:44:48 +0100

    REST API with Mongoose and MongoDB

---
+++ models/leaders.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose');
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const Schema = mongoose.Schema;
+
+var commentSchema = new Schema({
+    rating:  {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment:  {
+        type: String,
+        required: true
+    },
+    author:  {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+});
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+    abbr: {
+        type: String,
+        default: ''
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    featured: {
+        type: Boolean,
+        default:false      
+    },
+    comments:[commentSchema]
+}, {
+    timestamps: true
+});
+
+
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
