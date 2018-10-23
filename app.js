f044cc63cc2615ec27c9dc4409daf5a172a9002e
marcburnett <marc_burnett@yahoo.com>
2018-10-23 15:18:26 +0100

    Passport JWT

--- app.js
+++ app.js
@@ -4,17 +4,48 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+var config = require('./config');
+const url = config.mongoUrl;
+
 
 
 var dishRouter = require('./routes/dishRouter');
 var promotionRouter = require('./routes/promotionRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 const Dishes = require('./models/dishes');
 const Promotions = require('./models/promotions');
 const Leaders = require('./models/leaders');
 
+
+
 var app = express();
+
+//Express Sessions
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+function auth (req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      next(err);
+    }
+    else {
+          next();
+    }
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -29,6 +60,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes',dishRouter);
 app.use('/promotions',promotionRouter);
 app.use('/leaders',leaderRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -46,7 +79,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const url = 'mongodb://localhost:27017/conFusion';
+
+
+
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly to server");
